8af6151a39683c3801ef488d9e8c987cff646b4f
Jose Perez <jperezjimenez06gmail.com>
2026-03-20 08:19:15 -0400

    se agrego la parte de servicios

---
+++ servicios/interior-vehiculo/codigo/codigo.js
@@ -0,0 +1,17 @@
+const ancla = document.getElementById("btn-cta-wa");
+const telefono = "8294885016";
+const mensaje =
+  "Hola, me interesa el servicio de limpieza profunda de interior de vehiculo.";
+
+function generarLinkWhatsApp(telefono, mensaje) {
+  // Limpiar el número (solo números)
+  const numero = telefono.replace(/\D/g, "");
+
+  // Codificar el mensaje para URL
+  const texto = encodeURIComponent(mensaje);
+
+  // Crear el link
+  return `https://wa.me/${numero}?text=${texto}`;
+}
+
+ancla.setAttribute("href", generarLinkWhatsApp(telefono, mensaje));
---
+++ servicios/lavado-alfombras/codigo/codigo.js
@@ -0,0 +1,16 @@
+const ancla = document.getElementById("btn-cta-wa");
+const telefono = "8294885016";
+const mensaje = "Hola, me interesa el servicio de limpieza de alfombras.";
+
+function generarLinkWhatsApp(telefono, mensaje) {
+  // Limpiar el número (solo números)
+  const numero = telefono.replace(/\D/g, "");
+
+  // Codificar el mensaje para URL
+  const texto = encodeURIComponent(mensaje);
+
+  // Crear el link
+  return `https://wa.me/${numero}?text=${texto}`;
+}
+
+ancla.setAttribute("href", generarLinkWhatsApp(telefono, mensaje));
